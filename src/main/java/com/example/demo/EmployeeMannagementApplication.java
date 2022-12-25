94cba8d6593c946c7ab313707874bb498850c066
SUSOBHANLAL <susobhanlal2017@gmail.com>
2022-12-25 09:37:25 +0530

    first commit

---
+++ src/main/java/com/example/demo/EmployeeMannagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMannagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMannagementApplication.class, args);
+	}
+
+}
